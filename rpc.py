0e2dccafd03bea6345f838c390cc3ad607ee3910
Jason Barrella <brrjas002@myuct.ac.za>
2021-03-13 22:47:50 +0200

    get events

--- rpc.py
+++ rpc.py
@@ -10,7 +10,14 @@ def main():
     channel = grpc.insecure_channel(MAIN_NODE)
     stub = access_pb2_grpc.AccessAPIStub(channel)
     response = stub.GetLatestBlock(access_pb2.GetLatestBlockRequest())
-    print(response.block.height)
+    latest_block_heght = response.block.height
+
+    response = stub.GetEventsForHeightRange(access_pb2.GetEventsForHeightRangeRequest(
+        type='A.c1e4f4f4c4257510.Market.MomentPurchased',
+        start_height=latest_block_heght - 100,
+        end_height=latest_block_heght))
+
+    print(response)
 
 
 if __name__ == '__main__':
